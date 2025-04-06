e822562d6a69b8f62fa732a672a11421af548094
kzambrana <kzambrana0912@gmail.com>
2025-04-06 18:05:01 -0400

    Implement create account view for auth

---
+++ projects/linkify/src/app/features/auth/lf-create-account-view/lf-create-account-view.component.ts
@@ -0,0 +1,67 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {LfButtonComponent} from '../../../ui/lf-button/lf-button.component';
+import {LfInputComponent} from '../../../ui/lf-input/lf-input.component';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'lf-create-account-view',
+  imports: [
+    LfButtonComponent,
+    LfInputComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './lf-create-account-view.component.html',
+  styleUrl: './lf-create-account-view.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class LfCreateAccountViewComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  public getErrorMessage(controlName: string): string {
+    const control = this.form.get(controlName);
+
+    if (!control || !control.touched) {
+      return '';
+    }
+
+    if (control.hasError('required')) {
+      return 'Cant be empty';
+    }
+
+    if (controlName === 'email' && control.hasError('email')) {
+      return 'Please enter a valid email address';
+    }
+
+    if (controlName === 'password' && control.hasError('minlength')) {
+      return 'Password must be at least 8 characters';
+    }
+    if (controlName === 'confirmPassword' && this.form.get('confirmPassword')?.value !== this.form.get('password')?.value) {
+      return 'Passwords do not match';
+    }
+
+    return '';
+  }
+
+
+  public onInputValueChange(controlName: string, value: string): void {
+    this.form.get(controlName)?.setValue(value);
+    this.form.get(controlName)?.markAsTouched();
+    this.form.updateValueAndValidity();
+  }
+
+  public onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+    } else {
+      console.log('Valid form!');
+    }
+  }
+}
